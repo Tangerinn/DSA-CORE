9254c8e831979f8f192bf2f156db5aa28f7c3055
Tangerinn <tanayapawar248@gmail.com>
2024-08-06 14:21:44 +0530

    Update FindInMountainArray.cpp

--- FindInMountainArray.cpp
+++ FindInMountainArray.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
 // This function calculates in the increasing portion or in the decreasing protion (depends upon what is called)
 
@@ -15,7 +14,7 @@ int binarySearch(int start, int end, int target, vector<int> mount, bool increas
         if (target == mount[mid])
         {
             ans = mid; // saved answer
-            break; // Break out of the loop once target is found
+            break; // Break out of the (nearest) LOOP once target is found 
         }
 
         if (increasing == true) // true===> increasing
@@ -24,7 +23,7 @@ int binarySearch(int start, int end, int target, vector<int> mount, bool increas
             {
                 start = mid + 1;
             }
-            if (target < mount[mid])
+            else if (target < mount[mid])
             {
                 end = mid - 1;
             }
@@ -36,7 +35,7 @@ int binarySearch(int start, int end, int target, vector<int> mount, bool increas
             {
                 end = mid - 1;
             }
-            if (target < mount[mid])
+            else if (target < mount[mid])
             {
                 start = mid + 1;
             }
@@ -49,7 +48,7 @@ int binarySearch(int start, int end, int target, vector<int> mount, bool increas
 int main()
 {
     vector<int> mount = {0, 1, 2, 3, 4, 5, 7, 15, 11, 6, 4, 2, 1}; // sample mountain array
-    int target = 0;
+    int target =11;
 
     // finding peak
 
@@ -75,7 +74,9 @@ int main()
        ans= binarySearch( 0, peak, target, mount, true);
     if (ans == -1)
     {
-        ans = binarySearch(peak, mount.size() - 1, target, mount, false);
+        ans = binarySearch(peak+1, mount.size() - 1, target, mount, false); 
+        //"peak+1" : the "+1" is important. We have considered the peak'th element in the "icreasing portion" pf array. 
+        //So the decreasing part must start from "peak+1"th index.
     }
         cout << "Index of lowest occurence of " << target << " : " << ans;
 
