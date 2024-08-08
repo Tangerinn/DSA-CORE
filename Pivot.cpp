70aa5755bfe792c7a0eacf031e30b6b9ccbb6b8a
Tangerinn <tanayapawar248@gmail.com>
2024-08-08 12:25:44 +0530

    Update Pivot.cpp

--- Pivot.cpp
+++ Pivot.cpp
@@ -3,29 +3,25 @@
 
 using namespace std;
 
-//Finding pivot(max or min value in a rotated array) 
-//APPROACH : Compare the mid value with the start value and reduce search space accordingly. 
-int main()
-{
-    vector<int> nums = {3, 4, 5, 6, 7, 0, 1, 2}; // sample array (rotated)
-    // int target =6;
+// Finding pivot(min value in a rotated array)
+// APPROACH : Compare the mid value with the start value and reduce search space
+// accordingly.
+int main() {
+  vector<int> nums = {3, 4, 5, 6, 7, 0, 1, 2}; // sample array (rotated)
+  // int target =6;
 
-    int start = 0;
-    int end = nums.size() - 1;
-    int ans = -1;
+  int start = 0;
+  int end = nums.size() - 1;
 
-    while (start < end)
+  while (start < end) {
+    int mid = start + (end - start) / 2;
+
+    if (nums[mid] >= nums[start]) {
+      start = mid + 1;
+    } else // nums[mid]<nums[start]
     {
-        int mid = start + (end - start) / 2;
-
-        if (nums[mid] >= nums[start])
-        {
-            start = mid + 1;
-        }
-        else // nums[mid]<nums[start]
-        {
-            end = mid - 1;
-        }
+      end = mid;
     }
-    cout << "pivot : " << nums[start];
+  }
+  cout << "pivot : " << nums[start];
 }
